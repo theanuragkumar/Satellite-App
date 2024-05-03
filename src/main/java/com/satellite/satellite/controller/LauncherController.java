62d49826ba5e726602eea88df9b5e6c4a11d00e7
theanuragkumar <theanuragkumarofficial@gmail.com>
2024-05-03 08:21:27 +0530

    Added Cross Origin

--- src/main/java/com/satellite/satellite/controller/LauncherController.java
+++ src/main/java/com/satellite/satellite/controller/LauncherController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/launchers")
+@CrossOrigin()
 public class LauncherController {
     @Autowired
     private LauncherService launcherService;
